e2c46f0173f686ed5e5fcdc353fbf83126513f8b
Akhras <65845301+Akhras4@users.noreply.github.com>
2024-08-31 14:43:10 +0200

    save the pdf in AWS

--- controller/HomeController.js
+++ controller/HomeController.js
@@ -4,6 +4,7 @@ const path = require('path');
 const users = require("../models/user");
 const books = require("../models/book");
 require('dotenv').config();
+const AWS = require('aws-sdk');
 
 const PDF_FOLDER = path.join(__dirname, '..', 'public', 'pdfs');
 const IMAGES_FOLDER = path.join(__dirname, '..', 'public', 'images');
@@ -13,60 +14,90 @@ const PORT = process.env.PORT ;
 /* This code snippet defines a JavaScript object named `HomeController` which contains several methods
 for handling different routes and actions in a web application. Here is a breakdown of what each
 method does: */
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+});
 const HomeController = {
   index: (req, res) => {
     const userid = req.params.id;
-    users.findOne({_id:userid}) 
-    .then(user=>{books.find({})
-         .then(books=>{console.log(req.files);
-         res.render(`index`,{userid,books,errors:null,username:user.UserName});})
-         .catch(err=>{res.render('index',{ userid,books });})
-    }).catch(err=>{res.render('index',{userid })})
-    
+    users.findOne({ _id: userid })
+      .then(user => {
+        books.find({})
+          .then(books => {
+            console.log(req.files);
+            res.render(`index`, { userid, books, errors: null, username: user.UserName });
+          })
+          .catch(err => {
+            res.render('index', { userid, books });
+          });
+      })
+      .catch(err => {
+        res.render('index', { userid });
+      });
   },
-  
-  upload: (req, res) => { 
-    if (req.method==="POST"){ 
-     if (req.uploadError) {
-      const userid  = req.params.id
-      console.log(req.params)
-        return res.status(401).render("index", { errors: req.uploadError, userid  });
-    }else {
-     const userid = req.params.id;
-     const { title, description } = req.body;
-     const pdfPath = req.files['pdf'][0].path
-     const baseUrl = `http://localhost:${PORT}`
-     const imagePath= req.files['image'][0].path.replace(/\\/g, '/')
-     const imageUrl = baseUrl + '/' + imagePath;
-     console.log(req.files)
-     //console.log(imageUrl)
-     console.log(req.params)
-     const newbook=new books({
-      user_id:userid,
-      title:title,
-      description:description,
-      pdfPath:pdfPath,
-      imagePath:imageUrl
-     })
-     newbook.save()
-     .then(() => {
-         return users.findById(userid);
-     })
-     .then(user => {
-         if (!user) {
-             throw new Error("User not found");
-         }
-         user.books.push(newbook._id);
-         return user.save();
-     })
-     .then(() => {
-         res.redirect(`/user/${userid}`);
-     })
-     .catch(error => {
-         res.status(500).send('Internal server error');
-     });
-}
-}},
+
+  upload: (req, res) => {
+    if (req.method === "POST") {
+      if (req.uploadError) {
+        const userid = req.params.id;
+        console.log(req.params);
+        return res.status(401).render("index", { errors: req.uploadError, userid });
+      } else {
+        const userid = req.params.id;
+        const { title, description } = req.body;
+        const pdfFile = req.files['pdf'][0];
+        const imageFile = req.files['image'][0];
+        const pdfUploadParams = {
+          Bucket: process.env.AWS_S3_BUCKET_NAME,
+          Key: `pdfs/${pdfFile.filename}`, 
+          Body: fs.createReadStream(pdfFile.path),
+          ContentType: pdfFile.mimetype,
+          ACL: 'public-read' 
+        };
+
+        const imageUploadParams = {
+          Bucket: process.env.AWS_S3_BUCKET_NAME,
+          Key: `images/${imageFile.filename}`, // Key defines the file path in S3
+          Body: fs.createReadStream(imageFile.path),
+          ContentType: imageFile.mimetype,
+          ACL: 'public-read'
+        };
+        s3.upload(pdfUploadParams).promise()
+          .then(pdfData => {
+            // Upload Image to S3
+            return s3.upload(imageUploadParams).promise()
+              .then(imageData => ({ pdfUrl: pdfData.Location, imageUrl: imageData.Location }));
+          })
+          .then(({ pdfUrl, imageUrl }) => {
+            const newbook = new books({
+              user_id: userid,
+              title: title,
+              description: description,
+              pdfPath: pdfUrl,
+              imagePath: imageUrl
+            });
+            return newbook.save()
+              .then(() => users.findById(userid))
+              .then(user => {
+                if (!user) {
+                  throw new Error("User not found");
+                }
+                user.books.push(newbook._id);
+                return user.save();
+              });
+          })
+          .then(() => {
+            res.redirect(`/user/${userid}`);
+          })
+          .catch(error => {
+            console.error('Upload error:', error);
+            res.status(500).send('Internal server error');
+          });
+      }
+    }
+  },
 
 openPdf: (req, res) => {
   try {
